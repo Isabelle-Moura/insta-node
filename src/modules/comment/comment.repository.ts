81ffa96b2e175fc3ec412d441cab00d622065a4e
Isabelle-Moura <mourabisabelle@gmail.com>
2024-01-17 23:03:04 -0300

    fix: relationship between Comment, Like and Post.

--- src/modules/comment/comment.repository.ts
+++ src/modules/comment/comment.repository.ts
@@ -1,27 +1,36 @@
+import { PostModel } from "../post/post.model";
 import { CreateCommentDTO, UpdateCommentDTO } from "./comment.dto";
 import { CommentModel, Comment } from "./comment.model";
 import { Model } from "mongoose";
 
 export class CommentRepository {
    constructor(private model: Model<Comment>) {}
-   async createComment(comment: CreateCommentDTO): Promise<Comment> {
-      const newComment = (await this.model.create(comment)).populate("post");
-      return newComment;
+
+   async createComment(comment: CreateCommentDTO): Promise<Comment | null> {
+      // Cria um novo comentário
+      const newComment = await this.model.create(comment);
+
+      // Atualiza o post associado com o novo comentário usando $push
+      await PostModel.findByIdAndUpdate(
+         newComment.post, // Supondo que há um campo 'post' no modelo Comment que armazena o ID do post
+         { $push: { comments: newComment } },
+         { new: true }
+      );
+
+      // Use populate para preencher os detalhes do post no novo comentário
+      const populatedComment = await this.model.findById(newComment._id).populate("post");
+
+      return populatedComment;
    }
 
    async updateComment(commentId: string, updatedComment: UpdateCommentDTO): Promise<Comment | null> {
-      const comment = await this.model.findByIdAndUpdate(
-         commentId,
-         {
-            ...updatedComment,
-         },
-         { new: true }
-      );
-      return comment;
+      const updatedCommentDocument = await this.model.findByIdAndUpdate(commentId, { $push: { comments: updatedComment } }, { new: true });
+
+      return updatedCommentDocument;
    }
 
-   async getCommentById(CommentId: string): Promise<Comment | null> {
-      const Comment = await this.model.findById(CommentId);
-      return Comment;
+   async getCommentById(commentId: string): Promise<Comment | null> {
+      const comment = await this.model.findById(commentId);
+      return comment;
    }
 }
--- src/modules/like/like.repository.ts
+++ src/modules/like/like.repository.ts
@@ -1,3 +1,4 @@
+import { PostModel } from "../post/post.model";
 import { Like } from "./like.model";
 import { Model } from "mongoose";
 
@@ -5,9 +6,14 @@ export class LikeRepository {
    constructor(private model: Model<Like>) {}
 
    async createLike(postId: string): Promise<Like> {
+      // Cria um novo like
       const like = await this.model.create({
          post: postId,
       });
+
+      // Atualiza o post associado com o novo like usando $push
+      await PostModel.findByIdAndUpdate(postId, { $push: { likes: like } }, { new: true });
+
       return like;
    }
 }
